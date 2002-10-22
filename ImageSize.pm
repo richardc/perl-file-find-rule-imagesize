1780b95ed5f44c4b068f438a57c81d06aa51f4c1
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2002-10-22 11:04:16 +0000

    make ImageSize work
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/File-Find-Rule-ImageSize@819 c56e198b-7db7-0310-b3ab-8e406f684e33

--- ImageSize.pm
+++ ImageSize.pm
@@ -1,3 +1,4 @@
+# $Id: $
 package File::Find::Rule::ImageSize;
 use strict;
 use vars qw/$VERSION @EXPORT/;
@@ -7,11 +8,13 @@ use base qw( File::Find::Rule );
 $VERSION = '0.01';
 @EXPORT = @File::Find::Rule::EXPORT; # yes, re-export
 
-sub File::Find::Rule::magic {
+sub File::Find::Rule::image_x {
     my $self = shift()->_force_object;
+    my @rules = @_;
     $self->exec( sub {
-                     my $type = $mm->checktype_filename($_);
-                     for (@patterns) { return 1 if $type =~ m/$_/ }
+                     my ($x, $y) = imgsize($_);
+                     return unless defined $x;
+                     for (@rules) { return 1 if $x == $_ }
                      return;
                  } );
 }
--- t/File-Find-Rule-ImageSize.t
+++ t/File-Find-Rule-ImageSize.t
@@ -1,7 +1,10 @@
 #!perl -w
 use strict;
-use Test::More tests => 1;
+use Test::More tests => 2;
 
-use File::Find::Rule::MMagic;
-is_deeply( [ find( magic => 'image/*', maxdepth => 2, in => 't' ) ],
-           [ 't/happy-baby.JPG' ] );
+use File::Find::Rule::ImageSize;
+is_deeply( [ find( image_x => 640, maxdepth => 2, in => 't' ) ],
+           [ 't/happy-baby.JPG' ], "x == 640" );
+
+is_deeply( [ find( image_x => 641, maxdepth => 2, in => 't' ) ],
+           [ ] );

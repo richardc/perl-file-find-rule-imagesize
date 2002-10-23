6e9a29a8a52e6caf8ca4a9228366d1de4c161da7
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2002-10-23 18:14:36 +0000

    depend on Number::Compare
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/File-Find-Rule-ImageSize@839 c56e198b-7db7-0310-b3ab-8e406f684e33

--- ImageSize.pm
+++ ImageSize.pm
@@ -3,6 +3,7 @@ package File::Find::Rule::ImageSize;
 use strict;
 use vars qw/$VERSION @EXPORT/;
 use File::Find::Rule;
+use Number::Compare;
 use Image::Size;
 use base qw( File::Find::Rule );
 $VERSION = '0.01';
@@ -10,11 +11,11 @@ $VERSION = '0.01';
 
 sub File::Find::Rule::image_x {
     my $self = shift()->_force_object;
-    my @rules = @_;
+    my @rules = map { Number::Compare->new($_) } @_;
     $self->exec( sub {
                      my ($x, $y) = imgsize($_);
                      return unless defined $x;
-                     for (@rules) { return 1 if $x == $_ }
+                     for (@rules) { return 1 if $_->($x) }
                      return;
                  } );
 }

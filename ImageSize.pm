7372b62e3ee3e0a395e94deaa1484901889cd394
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2002-10-23 19:29:04 +0000

    abandon jay!
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/File-Find-Rule-ImageSize@840 c56e198b-7db7-0310-b3ab-8e406f684e33

--- ImageSize.pm
+++ ImageSize.pm
@@ -1,13 +1,15 @@
 # $Id$
 package File::Find::Rule::ImageSize;
 use strict;
-use vars qw/$VERSION @EXPORT/;
+
 use File::Find::Rule;
+use base qw( File::Find::Rule );
+use vars qw/$VERSION @EXPORT/;
+$VERSION = '0.01';
+@EXPORT = @File::Find::Rule::EXPORT;
+
 use Number::Compare;
 use Image::Size;
-use base qw( File::Find::Rule );
-$VERSION = '0.01';
-@EXPORT = @File::Find::Rule::EXPORT; # yes, re-export
 
 sub File::Find::Rule::image_x {
     my $self = shift()->_force_object;
